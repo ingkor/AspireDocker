55b33b8f46d749d3db275d572d1bcdcd0c57b2aa
Jakob Sorgeloos <jakob.sorgeloos@external.atlascopco.com>
2025-03-06 15:11:08 +0100

    bumped to 9.1.0

--- AspireApp1.AppHost/Program.cs
+++ AspireApp1.AppHost/Program.cs
@@ -3,7 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
-
 builder
     .AddProject<Projects.AspireApp1_Web>("webfrontend")
     .WithExternalHttpEndpoints()
